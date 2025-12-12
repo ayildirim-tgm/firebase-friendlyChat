031f8493429530c4b15269d23dacee5dbac7aa69
Aran Yildirim <ayildirim@student.tgm.ac.at>
2025-12-12 12:36:53 +0100

    Environment.ts changed

--- angularfire-start/src/environments/environment.ts
+++ angularfire-start/src/environments/environment.ts
@@ -1,12 +1,20 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
+    apiKey: "AIzaSyD6fm35cWXdeYBzxvoWoY3FQuBoF36SuB0",
+  authDomain: "friendlychat-878c4.firebaseapp.com",
+  projectId: "friendlychat-878c4",
+  storageBucket: "friendlychat-878c4.firebasestorage.app",
+  messagingSenderId: "980092139000",
+  appId: "1:980092139000:web:148d3eb4a971264d26d20d"
     measurementId: 'G-MEASUREMENT_ID',
   },
+};
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6fm35cWXdeYBzxvoWoY3FQuBoF36SuB0",
+  authDomain: "friendlychat-878c4.firebaseapp.com",
+  projectId: "friendlychat-878c4",
+  storageBucket: "friendlychat-878c4.firebasestorage.app",
+  messagingSenderId: "980092139000",
+  appId: "1:980092139000:web:148d3eb4a971264d26d20d"
 };
