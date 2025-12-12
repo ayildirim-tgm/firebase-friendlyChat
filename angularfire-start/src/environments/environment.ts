50bee57580380d30046a073c5af1f39d4bd91cb1
Aran Yildirim <ayildirim@student.tgm.ac.at>
2025-12-12 12:46:52 +0100

    Environment.ts gefixt

--- angularfire-start/src/environments/environment.ts
+++ angularfire-start/src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
   projectId: "friendlychat-878c4",
   storageBucket: "friendlychat-878c4.firebasestorage.app",
   messagingSenderId: "980092139000",
-  appId: "1:980092139000:web:148d3eb4a971264d26d20d"
+  appId: "1:980092139000:web:148d3eb4a971264d26d20d",
     measurementId: 'G-MEASUREMENT_ID',
   },
 };
